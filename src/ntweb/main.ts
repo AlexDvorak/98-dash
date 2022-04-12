a4b4b0f0d0d52a77a266197c1fab707d43fcba89
Alex <>
2022-04-12 18:37:50 -0500

    .

--- src/ntweb/main.ts
+++ src/ntweb/main.ts
@@ -8,27 +8,34 @@ if (!("WebSocket" in window)) {
 class NtTable {
     path: string;
     name: string;
-    subtables: NtTable[];
-    entries: NtEntry[];
+    subtables: Set<NtTable>;
+    entries: Set<NtEntry>;
 
     constructor(path: string) {
+        this.subtables = new Set();
+        this.entries = new Set();
         this.path = path;
-        this.name = path.slice(1).split("/")[-1];
+        this.name = path.slice(1).split("/").slice(-1)[0];
+        console.log(this.name);
     }
 
     get parent() {
-        return this.path.slice(1).split("/")[-2];
+        return this.path.split("/")[-2];
+    }
+
+    get parent_path() {
+        return this.path.split("/").slice(0, -1).join("/");
     }
 
     addSubtable(t: NtTable) {
         if (t.parent === this.name) {
-            this.subtables.push(t);
+            this.subtables.add(t);
         }
     }
 
-    addEntry(e: NtEntry) {
-        if (e.parent == this.name) {
-            this.entries.push(e);
+    updateEntry(e: NtEntry) {
+        if (e.parent_path === this.name) {
+            this.entries.add(e);
         }
     }
 }
@@ -42,16 +49,22 @@ class NtEntry {
         this.value = value;
     }
 
-    get parent() {
-        return this.key.slice(1).split("/")[-2];
+    get parent_path() {
+        return this.key.split("/").slice(0, -1).join("/");
     }
 }
 
 let table = new NT(window.location.host);
 
-table.addGlobalListener((k, v, is_new) => {
-    if (is_new) {
+let tables = new Map<string, NtTable>();
+
+table.addGlobalListener((k: string, v: any, is_new: boolean) => {
+    let e = new NtEntry(k, v);
+    if (!tables.has(e.parent_path)) {
+        let t = new NtTable(e.parent_path);
+        tables.set(t.path, t);
     } else {
-        $(`<p>'${k}' changed to '${v}'<br>`).appendTo("body");
+        tables.get(e.parent_path).updateEntry(e);
     }
+    console.log(tables);
 }, true);
