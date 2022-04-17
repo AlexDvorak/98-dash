626928d502dc08f2cb7ae39c4d84361c69c6c4aa
Alex <>
2022-04-17 15:43:38 -0500

    refactor main html table viewer script with new knowledge of jquery and javascript

--- src/main/main.ts
+++ src/main/main.ts
@@ -11,7 +11,7 @@ let tables = new Map<string, NtTable>();
 let entries = new Map<string, NtEntry>();
 
 table.addGlobalListener((k: string, v: any, is_new: boolean) => {
-    let e = new NtEntry(k, v);
+    const e = new NtEntry(k, v);
     entries.set(k, e);
 
     let parent_table = new NtTable(e.parent_path);
@@ -29,36 +29,38 @@ function nt_path_to_id(nt_path: string): string {
 
 function update_html_table(tables: Iterable<NtTable>) {
     for (let t of tables) {
-        let table_id = nt_path_to_id(t.path);
+        const table_id = nt_path_to_id(t.path);
         let table = $("#" + table_id);
 
         // create table if doesn't exists
-        console.debug(table, table_id);
-        if (document.getElementById(table_id) === null) {
+        if (table.length === 0) {
             $(`<h1>${t.name}</h1>`).appendTo("body");
             $("<table></table>")
                 .attr("id", table_id)
                 .attr("border", 1)
                 .appendTo("body");
+            table = $("#" + table_id);
         }
 
         // populate it with the entries
-        for (let e of t.entries) {
-            let key_id = nt_path_to_id(e.key);
+        for (let entry of t.entries) {
+            const key_id = nt_path_to_id(entry.key);
+            const entry_value_cell = $("#"+ key_id);
 
-            if (document.getElementById(`${key_id}`) === null) {
+            // create row for the entry if it doesn't exist
+            if (entry_value_cell.length === 0) {
                 // create the row for the entry since it doesn't exist
-                let new_row = $("<tr></tr>").appendTo(`#${table_id}`);
+                const new_row = $("<tr></tr>").appendTo(table);
 
                 // add the key and value pair into the row
-                $("<td></td>").text(e.key).appendTo(new_row);
+                $("<td></td>").text(entry.key).appendTo(new_row);
                 $("<td></td>")
-                    .text(e.value)
+                    .text(entry.value)
                     .attr("id", key_id)
                     .appendTo(new_row);
             } else {
                 // just update the cell with the new data
-                $("#" + key_id).text(e.value);
+                entry_value_cell.text(entry.value);
             }
         }
     }
